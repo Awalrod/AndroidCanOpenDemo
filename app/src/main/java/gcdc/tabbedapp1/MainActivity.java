01df0ff36d4cc8890b410d78779b9639974dc575
Alex Walrod <awalrod123@gmail.com>
2016-07-11 16:03:33 -0500

    Test Branch

--- app/src/main/java/gcdc/tabbedapp1/MainActivity.java
+++ app/src/main/java/gcdc/tabbedapp1/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;//change
+import android.view.ViewGroup;//change 1
 
 
 import android.widget.AdapterView;
