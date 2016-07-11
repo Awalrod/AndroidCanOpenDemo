dabb21f598d18596dff2e43ff554ec9b5c8a43d5
Alex Walrod <awalrod123@gmail.com>
2016-07-11 16:02:24 -0500

    Second Commit

--- app/src/main/java/gcdc/tabbedapp1/MainActivity.java
+++ app/src/main/java/gcdc/tabbedapp1/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewGroup;//change
 
 
 import android.widget.AdapterView;
